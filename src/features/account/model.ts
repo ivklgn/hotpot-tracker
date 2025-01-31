0499d342327f1bc70ef300781571b66fdd0a60dd
Kalagin Ivan <lulldev@yandex.ru>
2025-01-31 16:25:09 +0700

    fix: current team bug

--- src/features/account/model.ts
+++ src/features/account/model.ts
@@ -8,6 +8,12 @@ export const currentTeamIdAtom = atom<string | null>(null, 'currentTeamIdAtom').
   withLocalStorage('currentTeamIdAtom')
 );
 
+teamsSubscription.dataAtom.onChange((ctx, teams) => {
+  if (teams?.data?.teams && teams?.data?.teams?.length > 0) {
+    currentTeamIdAtom(ctx, teams?.data?.teams?.[0].id);
+  }
+});
+
 export const currentTeamAtom = atom((ctx) => {
   const currentTeamId = ctx.spy(currentTeamIdAtom);
   const teams = ctx.spy(teamsSubscription.dataAtom);
