b4b28a59ccce45327235580314b67df61617c8ee
ivklgn <ivklgn@proton.me>
2025-04-13 10:51:46 +0700

    ci(sentry): add tracing and perf

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,18 @@ import { getOriginalErrorStringifiedInfo } from './utils/sentry.ts';
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   enabled: import.meta.env.PROD,
+  // tracesSampleRate: 0.1,
+  sampleRate: 1,
+  integrations: [
+    Sentry.browserTracingIntegration({
+      enableInp: false,
+    }),
+    Sentry.browserProfilingIntegration(),
+    // Sentry.thirdPartyErrorFilterIntegration({
+    //   filterKeys: ['app name'],
+    //   behaviour: 'apply-tag-if-exclusively-contains-third-party-frames',
+    // }),
+  ],
   beforeSend: (event, hint) => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
