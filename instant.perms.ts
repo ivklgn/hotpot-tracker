34e81382321a43ec35b1b2e8ab56d82a606f0545
ivklgn <ivklgn@proton.me>
2025-06-12 15:01:53 +0300

    chore: rollback columns limits

--- instant.perms.ts
+++ instant.perms.ts
@@ -38,8 +38,7 @@ export default {
     bind: ['isMember', "auth.id in data.ref('teams.memberships.userId')"],
     allow: {
       view: 'isMember',
-      create: 'isMember',
-      // create: `isMember && size(data.ref('boards.columns.id')) <= ${tariffLimits.free.max_columns_per_board}`,
+      create: `isMember && size(data.ref('boards.columns.id')) <= ${tariffLimits.free.max_columns_per_board}`,
       delete: 'isMember',
       update: 'isMember',
     },
